b0b03a67e7f67dd8dbb01245c2cc7b4df8ac4da3
Daniel Castillo <danielcastillog92@gmail.com>
2021-04-29 12:02:39 +0200

    Connection to API done. Having issues with the render.

--- frontend-metal-db/src/Components/Dropdown/Dropdown.jsx
+++ frontend-metal-db/src/Components/Dropdown/Dropdown.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react';
 
-const Dropdown = props => {
-    
-    const [selectedValue, setSelectedValue ] = useState('');
+const Dropdown = props => {    
+
+    const dropdownChanged = e => {
+        props.changed(e.target.value);
+
+    }    
 
     return (
-        <div>
-            <select value={selectedValue} onChange={e => setSelectedValue(e.target.value)} >
-                {props.options.map((item, idx) => <option key={idx} value={item.value}>{item.name}</option>)}
-            </select>
-            <p>{selectedValue}</p>
+        <div className="col-sm-6 form-group row px-0">     
+            <label className="form-label col-sm-2">{props.label}</label>       
+            <select value={props.selectedValue} onChange={dropdownChanged} className="form-control form-control-sm col-sm-10">
+                <option key={0}>Select...</option>
+                {/* {props.options.map((item, idx) => <option key={idx + 1} value={item.id}>{item.name}</option>)} */}
+            </select>            
         </div>
-    )
+    );
 }
 
-export default Dropdown
+export default Dropdown;
